d351d289555859395d418b9dd59b062905c3ae11
Ujjwal Kumar <ujjwal509kumar@gmail.com>
2026-01-28 20:14:52 +0530

    integrated clerk and dark mode from shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { Button } from "@/components/ui/button"
+import { ThemeProvider } from "@/components/theme-provider"
+import { ModeToggle } from './components/mode-toggle'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
 import './App.css'
 
 function App() {
   return (
     <>
-      <p className='flex flex-col text-center text-stone-800'>Hello world</p>
-      <Button onClick={()=>alert("Hello world")} variant="outline">Click here</Button>
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <div>
+          <ModeToggle />
+          <h1>Your App Content</h1>
+          <UserButton />
+        </div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+      </ThemeProvider>
     </>
   )
 }
